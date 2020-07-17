9da50e34d2d47810cf4b4e1b62b26886cb25161d
rwang0110 <richardwang0110@Richards-Gaming-Laptop.localdomain>
2020-07-16 20:48:27 -0700

    added link to data

---
+++ segmentation.py
@@ -0,0 +1,11 @@
+""" this document is meant to segmenet the data into sets of local neighborhoods""" 
+import numpy as np
+import pandas as pd
+
+
+def segment_data(datafile):
+    return
+
+if __name__ == "__main__":
+    #data = preprocess.txt_to_pandas("data/36_N_First_RCS.txt")
+  
